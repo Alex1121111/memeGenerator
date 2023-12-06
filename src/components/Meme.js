537e310786f4af73a62468d566d1c0f8d6ca983d
Pesala Sri Datta <datta.pesala@gmail.com>
2023-12-06 07:13:46 +0530

    Demo Api Requests and image div

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import data from './data'
 
 export default function Meme(){
     function handleClick(){
         console.log("I was clicked!")
+        const memeArray = data.memes
+        const r = Math.floor(Math.random()* memeArray.length)
+        console.log(data.memes[r])
+        // data.map(
+        //     d => console.log(d)
+        // );
     }
     return(
         <section className='meme-section'>
@@ -12,6 +19,15 @@ export default function Meme(){
             </div>
             <div className = 'button-section'>
                 <button onClick={handleClick}>Get a new meme image</button>
+            </div>
+            <div className='meme-img'>
+                    <div className = "meme-container">
+                    <img  className = "meme-image" src = {`${process.env.PUBLIC_URL}/images/meme1.svg`}/>
+                    </div>
+                    <div className = "overlay">
+                    <h1>This is a Test String</h1>
+                    <h2>This is a Test String 2</h2>
+                    </div>
             </div>
         </section>
     )
---
+++ src/components/data.js
@@ -0,0 +1,25 @@
+export default {
+    "success":true,
+    "memes":[
+    {
+        id: 1,
+        imagesrc: "meme1.svg",
+        htextp: "shut up",
+        htexts: "and take my money"
+    },
+
+    {
+        id: 2,
+        imagesrc: "meme2.jpg",
+        htextp: "",
+        htexts: ""
+    },
+
+    {
+        id: 3,
+        imagesrc: "meme3.jpg",
+        htextp: "",
+        htexts: ""
+    }
+]
+}
